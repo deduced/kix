801593262e50b99ab1bf313a4054a33362101f62
Charlie Astrada <carlosastrada@gmail.com>
2018-12-11 23:43:40 -0300

    Setup Apollo client HOC and wrap application with Apollo Provider
    * invoke Next hook getInitialProps to crawl pages and fetch queries/mutations to work with server-side rendering.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,18 +1,35 @@
+import React from "react";
 import App, { Container } from "next/app";
+import { ApolloProvider } from "react-apollo";
 import Page from "../components/Page";
+import withData from "../lib/withData";
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    //crawl pages and fetch data to work with server-side rendering
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    //exposes query to user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
   render() {
-    const { Component } = this.props;
+    const { Component, apollo, pageProps } = this.props;
 
     return (
       <Container>
-        <Page>
-          <Component />
-        </Page>
+        <ApolloProvider client={apollo}>
+          <Page>
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
