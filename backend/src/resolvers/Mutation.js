a8b89b4cbed0cc7a1176a42fdf4c7b2629ee9959
Charlie Astrada <carlosastrada@gmail.com>
2018-12-11 23:15:33 -0300

    Add createItem mutation and items query for Yoga.
    * Write mutation resolvers for createItems
    * Write query resolvers for items

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,23 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    //TODO implement authentication
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
 
-module.exports = Mutations; 
+    return item;
+  }
+
+  // createDog(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+
+  //   const newDog = { name: args.name };
+  //   global.dogs.push(newDog);
+  //   return newDog;
+  // }
+};
+
+module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,4 +1,15 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
 
-module.exports = Query; 
+const Query = {
+  //Forwards query to db. Works since query is same in prisma as in yoga
+  items: forwardTo("db")
 
+  //Does same as above but verbose (no forwarding)
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+
+  //   return items;
+  // }
+};
+
+module.exports = Query;
