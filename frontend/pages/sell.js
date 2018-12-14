4f784711a6cbfea3b7428ddae7ffcb337bee322c
Charlie Astrada <carlosastrada@gmail.com>
2018-12-14 17:23:00 -0300

    Refactor pages/sell.js to render CreateItem component rather than placeholder text.

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Link from "next/link";
+import CreateItem from "../components/CreateItem";
 
 const Sell = props => (
   <div>
-    <p>Selling!</p>
+    <CreateItem />
   </div>
 );
 
