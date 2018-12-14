35240aaa01187874f6de3092d26382e3a3b549ff
Charlie Astrada <carlosastrada@gmail.com>
2018-12-14 17:23:39 -0300

    Write CreateItem component and set up input for Title.
    * handleChange works with any input type

---
+++ frontend/components/CreateItem.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
+
+class CreateItem extends Component {
+  state = {
+    title: "",
+    description: "",
+    image: "",
+    largeImage: "",
+    price: 0
+  };
+
+  handleChange = event => {
+    const { name, type, value } = event.target;
+    const val = type === "number" ? parseFloat(value) : value;
+
+    this.setState({ [name]: val });
+  };
+
+  render() {
+    return (
+      <Form>
+        <fieldset>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Title"
+            required
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+        </fieldset>
+      </Form>
+    );
+  }
+}
+
+export default CreateItem;
