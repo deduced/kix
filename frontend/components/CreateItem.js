73bcb2a88ac8496cb26387f1e30ef5b102500c2a
Charlie Astrada <carlosastrada@gmail.com>
2018-12-14 17:33:44 -0300

    Add Price and Description inputs to CreateItem
    * Add Submit button with preventDefault()

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -22,18 +22,52 @@ class CreateItem extends Component {
 
   render() {
     return (
-      <Form>
+      <Form
+        onSubmit={e => {
+          e.preventDefault();
+          console.log(this.state);
+        }}
+      >
         <fieldset>
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            id="title"
-            name="title"
-            placeholder="Title"
-            required
-            value={this.state.title}
-            onChange={this.handleChange}
-          />
+          <label htmlFor="title">
+            Title
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Title"
+              required
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="price">
+            Price
+            <input
+              type="number"
+              id="price"
+              name="price"
+              placeholder="Price"
+              required
+              value={this.state.price}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <label htmlFor="description">
+            Description
+            <textarea
+              id="description"
+              name="description"
+              placeholder="Enter a description"
+              required
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
+          </label>
+
+          <button type="submit">Submit</button>
         </fieldset>
       </Form>
     );
