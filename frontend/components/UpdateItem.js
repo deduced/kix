db0062acbdfa0ac9138ae3889f977d6cf7253a77
Charlie Astrada <carlosastrada@gmail.com>
2018-12-15 19:47:26 -0300

    Create UpdateItem component (INCOMPLETE)
    * Add query for querying data for selected item
    * Remove superfluous methods and form items since UpdateItem code was copied form CreateItem component

---
+++ frontend/components/UpdateItem.js
@@ -0,0 +1,125 @@
+import React, { Component } from "react";
+import { Mutation, Query } from "react-apollo";
+import gql from "graphql-tag";
+import Router from "next/router";
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
+import ErrorMessage from "./ErrorMessage";
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: { id: $id }) {
+      id
+      title
+      description
+      price
+    }
+  }
+`;
+
+const UPDATE_ITEM_MUTATION = gql`
+  mutation UPDATE_ITEM_MUTATION(
+    $title: String!
+    $description: String!
+    $price: Int!
+    $image: String
+    $largeImage: String
+  ) {
+    createItem(
+      title: $title
+      description: $description
+      price: $price
+      image: $image
+      largeImage: $largeImage
+    ) {
+      id
+    }
+  }
+`;
+
+class UpdateItem extends Component {
+  state = {};
+
+  handleChange = event => {
+    const { name, type, value } = event.target;
+    const val = type === "number" ? parseFloat(value) : value;
+
+    this.setState({ [name]: val });
+  };
+
+  render() {
+    return (
+      <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
+        {({ data, loading }) => {
+          if (loading) return <p>Loading...</p>;
+
+          return (
+            <Mutation mutation={UPDATE_ITEM_MUTATION} variables={this.state}>
+              {(createItem, { loading, error }) => (
+                <Form
+                  onSubmit={async e => {
+                    e.preventDefault();
+                    const response = await createItem();
+                    Router.push({
+                      pathname: "/item",
+                      query: { id: response.data.createItem.id }
+                    });
+                  }}
+                >
+                  {/* ErrorMessage will only render if there is an error */}
+                  <ErrorMessage error={error} />
+
+                  {/* On submit, we disable the form and start the loading animation */}
+                  <fieldset disabled={loading} aria-busy={loading}>
+                    <label htmlFor="title">
+                      Title
+                      <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Title"
+                        required
+                        value={this.state.title}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+
+                    <label htmlFor="price">
+                      Price
+                      <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        placeholder="Price"
+                        required
+                        value={this.state.price}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+
+                    <label htmlFor="description">
+                      Description
+                      <textarea
+                        id="description"
+                        name="description"
+                        placeholder="Enter a description"
+                        required
+                        value={this.state.description}
+                        onChange={this.handleChange}
+                      />
+                    </label>
+
+                    <button type="submit">Submit</button>
+                  </fieldset>
+                </Form>
+              )}
+            </Mutation>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default UpdateItem;
+export { UPDATE_ITEM_MUTATION };
