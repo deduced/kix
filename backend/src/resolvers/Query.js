68ab3f02cc1e0e5a09c57f4c8001834ec21a1636
Charlie Astrada <carlosastrada@gmail.com>
2019-02-08 13:31:01 +0100

    Add query to resolve userId from request
    The express middleware parses the JWT and passes the userId to the request. Add schema and query resolver which permits application to query user from database using the userId.
    
    * Me added to schema for Query type
    * me resolver added to Query.js -- runs check to ensure userId exists (user is logged in).

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -4,14 +4,21 @@ const Query = {
   //Forwards query to db. Works since query is same in prisma as in yoga
   items: forwardTo("db"),
   item: forwardTo("db"),
-  itemsConnection: forwardTo("db")
+  itemsConnection: forwardTo("db"),
+  me: function(parent, args, ctx, info) {
+    // userId should be parsed and passed in by express middleware if user logged in
+    // exits if no userId
+    if (!ctx.request.userId) {
+      return null;
+    }
 
-  //Does same as above but verbose (no forwarding)
-  // async items(parent, args, ctx, info) {
-  //   const items = await ctx.db.query.items();
-
-  //   return items;
-  // }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
