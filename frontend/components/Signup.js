00538349b7880cf0a9a18c64be08def34d5c7600
Charlie Astrada <carlosastrada@gmail.com>
2019-02-08 12:44:01 +0100

    Signup component accepts user signups with email, name, and password
    * Form uses method="post" as a fallback to avoid showing password in URL (and inadvertently saving it)
    *

--- frontend/components/Signup.js
+++ frontend/components/Signup.js
@@ -4,14 +4,84 @@ import gql from "graphql-tag";
 import Form from "./styles/Form";
 import Error from "./ErrorMessage";
 
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    signup(email: $email, name: $name, password: $password) {
+      id
+      email
+      name
+    }
+  }
+`;
+
 class Signup extends Component {
+  state = {
+    name: "",
+    password: "",
+    email: ""
+  };
+
+  saveToState = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
   render() {
     return (
-      <Form>
-        <fieldset>
-          <h2>Signup for an Account</h2>
-        </fieldset>
-      </Form>
+      <Mutation mutation={SIGNUP_MUTATION} variables={this.state}>
+        {(signup, { error, loading }) => {
+          return (
+            <Form
+              method="post"
+              onSubmit={event => {
+                event.preventDefault();
+                signup();
+              }}
+            >
+              <fieldset disabled={loading} aria-busy={loading}>
+                <h2>Signup for an Account</h2>
+                <Error error={error} />
+                <label htmlFor="email">
+                  Email
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    value={this.state.email}
+                    onChange={this.saveToState}
+                  />
+                </label>
+                <label htmlFor="name">
+                  Name
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="name"
+                    value={this.state.name}
+                    onChange={this.saveToState}
+                  />
+                </label>
+                <label htmlFor="password">
+                  Password
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                    value={this.state.password}
+                    onChange={this.saveToState}
+                  />
+                </label>
+                <button type="submit">Sign Up!</button>
+              </fieldset>
+            </Form>
+          );
+        }}
+      </Mutation>
     );
   }
 }
